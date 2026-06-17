da922e6a2a66509595679df987b443f64928a017
zNitche <53448802+zNitche@users.noreply.github.com>
2026-06-17 21:58:15 +0200

    refactor: tsconfig cleanup

--- eslint.config.js
+++ eslint.config.js
@@ -17,7 +17,7 @@ export default defineConfig([
         ],
         languageOptions: {
             parserOptions: {
-                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                project: ['./tsconfig.json'],
                 tsconfigRootDir: import.meta.dirname,
             },
         },
