9dd44f6b666f7f91427f2ebcd8af1eb230fc0195
zNitche <53448802+zNitche@users.noreply.github.com>
2026-06-17 21:16:35 +0200

    feat: basic example app setup

---
+++ example/eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+    globalIgnores(['node_modules', "./public",]),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommendedTypeChecked,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+            tseslint.configs.stylisticTypeChecked,
+        ],
+        languageOptions: {
+            parserOptions: {
+                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+])
---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import "moranaui/styles.css";
+import { MoranaTest } from "moranaui";
+
+export default function App() {
+    return (
+        <>
+            <MoranaTest />
+        </>
+    )
+}
