35349b6bc3c119444fa656721f772aa0144fc3a5
zNitche <53448802+zNitche@users.noreply.github.com>
2026-06-17 21:15:23 +0200

    refactor: eslint tweaks

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,25 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-    globalIgnores(['node_modules', "./public"]),
+    globalIgnores(['node_modules', "./public",]),
     {
         files: ['**/*.{ts,tsx}'],
         extends: [
             js.configs.recommended,
-            tseslint.configs.recommended,
+            tseslint.configs.recommendedTypeChecked,
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
+            tseslint.configs.stylisticTypeChecked,
         ],
         languageOptions: {
-            globals: globals.browser,
+            parserOptions: {
+                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
     },
 ])
