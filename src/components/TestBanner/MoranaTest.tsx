11ab2cc240033ba5e1b8d64626ea351b83378804
zNitche <53448802+zNitche@users.noreply.github.com>
2026-06-25 16:50:51 +0000

    refactor: removed placeholder component

--- src/components/TestBanner/MoranaTest.tsx
+++
@@ -1,9 +0,0 @@
-import classes from "./MoranaTest.module.css";
-
-interface MoranaTestProps {
-    readonly text: string;
-}
-
-export default function MoranaTest({ text }: MoranaTestProps) {
-    return <div className={classes.moranaTest}>Hello {text}</div>;
-}
