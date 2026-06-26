82012b476dd54fbad61a66e8b8e16d1590bd5f97
zNitche <53448802+zNitche@users.noreply.github.com>
2026-06-26 17:56:24 +0000

    feat: updated example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import "moranaui/styles.css";
-import { Router, Route } from "moranaui";
+// import classes from "./App.module.css";
+import { Router, Route, MoranaApp } from "moranaui";
 import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import CatchAllPage from "./pages/CatchAllPage/CatchAllPage";
@@ -9,24 +10,33 @@ import ParamsPage from "./pages/ParamsPage/ParamsPage";
 
 export default function App() {
     return (
-        <Router>
-            <Route
-                url={"/"}
-                component={HomePage}
-            />
-            <Route
-                url={"/about"}
-                component={AboutPage}
-            />
-             <Route
-                url={"/test/:id/param/:w/:q/pass"}
-                component={ParamsPage}
-            />
-            <Route
-                url="*"
-                wrapper={CatchAllWrapper}
-                component={CatchAllPage}
-            />
-        </Router>
+        <MoranaApp
+            // navAnimationBuilder={{
+            //     duration: 1000,
+            //     wrapperClassName: classes.navWrapper,
+            //     enterAnimation: classes.navEnterAnimation,
+            //     exitAnimation: classes.navExitAnimation,
+            // }}
+        >
+            <Router>
+                <Route
+                    url={"/"}
+                    component={HomePage}
+                />
+                <Route
+                    url={"/about"}
+                    component={AboutPage}
+                />
+                <Route
+                    url={"/test/:id/param/:w/:q/pass"}
+                    component={ParamsPage}
+                />
+                <Route
+                    url="*"
+                    wrapper={CatchAllWrapper}
+                    component={CatchAllPage}
+                />
+            </Router>
+        </MoranaApp>
     );
 }
