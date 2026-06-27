ecfa329065f821d505cb2d5f24ad3be8a842f295
zNitche <53448802+zNitche@users.noreply.github.com>
2026-06-27 11:48:57 +0000

    feat: morana content & header - page components registry integration

--- src/core/components/MoranaContent/MoranaContent.tsx
+++ src/core/components/MoranaContent/MoranaContent.tsx
@@ -1,16 +1,40 @@
-import type { PropsWithChildren } from "react";
+import { useEffect, type PropsWithChildren } from "react";
 import classes from "./MoranaContent.module.css";
 import { clsx } from "@root/utils";
+import useMoranaPageContext from "@root/core/hooks/useMoranaPageContext";
 
 interface MoranaContentProps {
     readonly className?: string;
+    readonly childrenClassName?: string;
 }
 
 export default function MoranaContent({
     children,
     className,
+    childrenClassName,
 }: PropsWithChildren<MoranaContentProps>) {
+    const {
+        classForNavState,
+        shouldAnimatePage,
+        updatePageStructuralComponentsRegistry,
+    } = useMoranaPageContext();
+
+    useEffect(() => {
+        updatePageStructuralComponentsRegistry("content", true);
+
+        return () => updatePageStructuralComponentsRegistry("content", false);
+    }, []);
+
     return (
-        <div className={clsx(classes.moranaContent, className)}>{children}</div>
+        <div className={clsx(classes.moranaContent, className)}>
+            <div
+                className={clsx(
+                    !shouldAnimatePage && classForNavState,
+                    childrenClassName,
+                )}
+            >
+                {children}
+            </div>
+        </div>
     );
 }
--- src/core/components/MoranaHeader/MoranaHeader.tsx
+++ src/core/components/MoranaHeader/MoranaHeader.tsx
@@ -1,16 +1,40 @@
-import type { PropsWithChildren } from "react";
+import { useEffect, type PropsWithChildren } from "react";
 import classes from "./MoranaHeader.module.css";
 import { clsx } from "@root/utils";
+import useMoranaPageContext from "@root/core/hooks/useMoranaPageContext";
 
 interface MoranaHeaderProps {
     readonly className?: string;
+    readonly childrenClassName?: string;
 }
 
 export default function MoranaHeader({
     children,
     className,
+    childrenClassName,
 }: PropsWithChildren<MoranaHeaderProps>) {
+    const {
+        classForNavState,
+        shouldAnimatePage,
+        updatePageStructuralComponentsRegistry,
+    } = useMoranaPageContext();
+
+    useEffect(() => {
+        updatePageStructuralComponentsRegistry("header", true);
+
+        return () => updatePageStructuralComponentsRegistry("header", false);
+    }, []);
+
     return (
-        <div className={clsx(classes.moranaHeader, className)}>{children}</div>
+        <div className={clsx(classes.moranaHeader, className)}>
+            <div
+                className={clsx(
+                    !shouldAnimatePage && classForNavState,
+                    childrenClassName,
+                )}
+            >
+                {children}
+            </div>
+        </div>
     );
 }
