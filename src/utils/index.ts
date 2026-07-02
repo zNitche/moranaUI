59da85b2f3cec1635ec9a106bdf8e0672619cf34
zNitche <53448802+zNitche@users.noreply.github.com>
2026-07-02 18:29:24 +0000

    feat: sleep & range utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,9 @@ export { generateUUID };
 
 import { buildUrl } from "./url";
 export { buildUrl };
+
+import { sleep } from "./sleep";
+export { sleep };
+
+import { range } from "./range";
+export { range };
---
+++ src/utils/range.ts
@@ -0,0 +1,3 @@
+export function range(iters: number) {
+    return Array.from(Array(iters).keys());
+}
