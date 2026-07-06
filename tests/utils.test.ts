bf6698597e64861904564c8c37cb5ee22100a59d
zNitche <53448802+zNitche@users.noreply.github.com>
2026-07-06 17:07:42 +0000

    test: added tests for all utils

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,13 @@
 import { expect, test } from "vitest";
-import { clsx, generateUUID, range } from "../src/utils";
+import clsx from "../src/utils/clsx";
+import { generateUUID } from "../src/utils/crypto";
+import { range } from "../src/utils/range";
+import {
+    splitPath,
+    tokenizeUrl,
+    buildUrl,
+    matchTokenizedUrl,
+} from "../src/utils/url";
 
 test("generate UUID", () => {
     const r = () => {
@@ -21,4 +29,61 @@ test("test range() utils", () => {
 
     expect(s).toStrictEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     expect(s).toHaveLength(10);
+});
+
+test("test url utils - splitPath()", () => {
+    const s = splitPath("/test/route/123?query=123");
+
+    expect(s).toStrictEqual(["test", "route", "123"]);
+    expect(s).toHaveLength(3);
+});
+
+test("test url utils - buildUrl()", () => {
+    const s = buildUrl("/test/route/123", { query: "123" });
+
+    expect(s).toEqual("/test/route/123?query=123");
+});
+
+test("test url utils - tokenizeUrl()", () => {
+    const s = tokenizeUrl("/test/:val/details?query=123");
+
+    expect(s).toStrictEqual([
+        {
+            isPathParam: false,
+            pathParamName: undefined,
+            token: "test",
+        },
+        {
+            isPathParam: true,
+            pathParamName: "val",
+            token: ":val",
+        },
+        {
+            isPathParam: false,
+            pathParamName: undefined,
+            token: "details",
+        },
+    ]);
+    expect(s).toHaveLength(3);
+});
+
+test("test url utils - matchTokenizedUrl()", () => {
+    const tokenizedUrl = tokenizeUrl("/test/:val/details?query=123");
+
+    expect(tokenizeUrl).toBeDefined();
+
+    const matchingUrl = matchTokenizedUrl(tokenizedUrl!, "/test/123/details");
+    const notMatchingUrl = matchTokenizedUrl(tokenizedUrl!, "/test/details");
+
+    expect(matchingUrl).toStrictEqual({
+        isMatching: true,
+        pathParams: {
+            val: "123",
+        },
+    });
+
+    expect(notMatchingUrl).toStrictEqual({
+        isMatching: false,
+        pathParams: undefined,
+    });
 });
