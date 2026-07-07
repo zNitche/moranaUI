4ea75579fa59f87cde2c78df1f6928156e2fc728
zNitche <53448802+zNitche@users.noreply.github.com>
2026-07-07 16:20:08 +0000

    chore: project structural changes - integration of npm workspaces

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,11 @@
 import "./index.css";
 import "moranaui/styles.css";
-// import classes from "./App.module.css";
-import { Router, Route, MoranaApp } from "moranaui";
+import {
+    Router,
+    Route,
+    MoranaApp,
+    MoranaRoutesWrapper,
+} from "moranaui";
 import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import CatchAllPage from "./pages/CatchAllPage/CatchAllPage";
@@ -12,24 +16,26 @@ export default function App() {
     return (
         <MoranaApp>
             <Router>
-                <Route
-                    url={"/"}
-                    component={HomePage}
-                />
-                <Route
-                    url={"/about"}
-                    component={AboutPage}
-                />
-                <Route
-                    url={"/test/:id/param/:w/:q/pass"}
-                    component={ParamsPage}
-                />
-                <Route
-                    url="*"
-                    cacheable={false}
-                    wrapper={CatchAllWrapper}
-                    component={CatchAllPage}
-                />
+                <MoranaRoutesWrapper>
+                    <Route
+                        url={"/"}
+                        component={HomePage}
+                    />
+                    <Route
+                        url={"/about"}
+                        component={AboutPage}
+                    />
+                    <Route
+                        url={"/test/:id/param/:w/:q/pass"}
+                        component={ParamsPage}
+                    />
+                    <Route
+                        url="*"
+                        cacheable={false}
+                        wrapper={CatchAllWrapper}
+                        component={CatchAllPage}
+                    />
+                </MoranaRoutesWrapper>
             </Router>
         </MoranaApp>
     );
