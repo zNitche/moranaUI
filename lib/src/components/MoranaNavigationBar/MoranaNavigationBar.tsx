730fc8066354305cfa3b52e90b6c22b6fd08b070
zNitche <53448802+zNitche@users.noreply.github.com>
2026-07-08 18:43:38 +0000

    feat: MoranaNavigationBar - hidden styling

--- lib/src/components/MoranaNavigationBar/MoranaNavigationBar.tsx
+++ lib/src/components/MoranaNavigationBar/MoranaNavigationBar.tsx
@@ -1,6 +1,7 @@
-import type { PropsWithChildren } from "react";
+import { useRef, type CSSProperties, type PropsWithChildren } from "react";
 import classes from "./MoranaNavigationBar.module.css";
 import { clsx } from "@root/utils";
+import useMoranaAppContext from "@root/core/hooks/context/useMoranaAppContext";
 
 interface MoranaNavigationBarProps {
     readonly className?: string;
@@ -10,8 +11,25 @@ export default function MoranaNavigationBar({
     className,
     children,
 }: PropsWithChildren<MoranaNavigationBarProps>) {
+    const { isNavbarVisible } = useMoranaAppContext();
+
+    const ref = useRef<HTMLDivElement>(null);
+
     return (
-        <div className={clsx(classes.moranaNavigationBar, className)}>
+        <div
+            ref={ref}
+            className={clsx(
+                classes.moranaNavigationBar,
+                className,
+                !isNavbarVisible && classes.hidden,
+            )}
+            style={
+                {
+                    // eslint-disable-next-line react-hooks/refs
+                    "--height": `${ref.current?.clientHeight}px`,
+                } as CSSProperties
+            }
+        >
             {children}
         </div>
     );
