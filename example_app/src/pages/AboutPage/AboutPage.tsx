1aa8e5ca198fe097bccf635b0b4707e66b5f9528
zNitche <53448802+zNitche@users.noreply.github.com>
2026-07-09 21:47:51 +0200

    feat: updated lib components exports + updated example app

--- example_app/src/pages/AboutPage/AboutPage.tsx
+++ example_app/src/pages/AboutPage/AboutPage.tsx
@@ -6,12 +6,20 @@ import {
     useIsPageActive,
     MoranaHeader,
     MoranaContent,
+    Modal,
+    FullScreenOverlay,
+    Drawer,
 } from "moranaui";
 import Header from "../../components/Header/Header";
 import Content from "../../components/Content/Content";
+import { useState } from "react";
 
 export default function AboutPage() {
     const { navigateTo } = useRouter();
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     const isPageActive = useIsPageActive();
 
@@ -35,9 +43,51 @@ export default function AboutPage() {
                         >
                             nav to home
                         </div>
+                        <div
+                            onClick={() =>
+                                setIsModalOpen((current) => !current)
+                            }
+                        >
+                            toggle modal
+                        </div>
+                        <div
+                            onClick={() =>
+                                setIsOverlayOpen((current) => !current)
+                            }
+                        >
+                            toggle overlay
+                        </div>
+                        <div
+                            onClick={() =>
+                                setIsDrawerOpen((current) => !current)
+                            }
+                        >
+                            toggle drawer
+                        </div>
                     </div>
                 </Content>
             </MoranaContent>
+
+            <FullScreenOverlay
+                isOpen={isOverlayOpen}
+                setIsOpen={setIsOverlayOpen}
+            >
+                test overlay
+            </FullScreenOverlay>
+
+            <Modal
+                isOpen={isModalOpen}
+                setIsOpen={setIsModalOpen}
+            >
+                test modal
+            </Modal>
+
+            <Drawer
+                isOpen={isDrawerOpen}
+                setIsOpen={setIsDrawerOpen}
+            >
+                test drawer
+            </Drawer>
         </MoranaPage>
     );
 }
--- lib/src/components/index.ts
+++ lib/src/components/index.ts
@@ -1,2 +1,11 @@
 import MoranaNavigationBar from "./MoranaNavigationBar/MoranaNavigationBar";
 export { MoranaNavigationBar };
+
+import Modal from "./Modal/Modal";
+export { Modal };
+
+import FullScreenOverlay from "./FullScreenOverlay/FullScreenOverlay";
+export { FullScreenOverlay };
+
+import Drawer from "./Drawer/Drawer";
+export { Drawer };
