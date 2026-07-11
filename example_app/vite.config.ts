e8c2bad617967afc58bf2d32489ab9b1918dd349
zNitche <53448802+zNitche@users.noreply.github.com>
2026-07-11 20:11:04 +0000

    build: example app build toolchain tweaks

--- example_app/vite.config.ts
+++ example_app/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            "@": resolve(__dirname, "./src"),
+            "@root": resolve(__dirname, "./src"),
         },
     },
 });
