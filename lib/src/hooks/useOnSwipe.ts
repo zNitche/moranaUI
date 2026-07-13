54589288d75f28b5702752c8f50e531ce7858584
zNitche <53448802+zNitche@users.noreply.github.com>
2026-07-13 19:28:02 +0000

    feat: useOnSwipe hook

---
+++ lib/src/hooks/useOnSwipe.ts
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import useDrag from "./useDrag";
+
+interface HookUseOnSwipeProps {
+    readonly onSwipe?: () => void;
+    readonly minMaxAngle: { min: number; max: number };
+    readonly minDragDistance: number;
+}
+
+export default function useOnSwipe<T>({
+    onSwipe,
+    minMaxAngle,
+    minDragDistance,
+}: HookUseOnSwipeProps) {
+    const { binds, dragDetails, dragInProgress } = useDrag<T>();
+
+    console.log(dragDetails)
+
+    useEffect(() => {
+        if (!dragInProgress || !dragDetails) {
+            return;
+        }
+
+        if (dragDetails.distanceFromStart >= minDragDistance) {
+            if (
+                dragDetails.angleDiff >= minMaxAngle.min &&
+                dragDetails.angleDiff <= minMaxAngle.max
+            ) {
+                onSwipe?.();
+            }
+        }
+    }, [
+        dragDetails,
+        dragInProgress,
+        minDragDistance,
+        minMaxAngle.max,
+        minMaxAngle.min,
+        onSwipe,
+    ]);
+
+    return { binds };
+}
