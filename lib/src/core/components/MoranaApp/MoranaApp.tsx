3bfa0b43a94bb1ce4f5e3c0b452e097da7de880c
zNitche <53448802+zNitche@users.noreply.github.com>
2026-07-17 21:41:50 +0200

    refactor: MoranaApp - changed navAnimationBuilder accepted props

--- lib/src/core/components/MoranaApp/MoranaApp.tsx
+++ lib/src/core/components/MoranaApp/MoranaApp.tsx
@@ -2,21 +2,40 @@ import { useMemo, useState, type PropsWithChildren } from "react";
 import type NavAnimationBuilder from "@root/types/NavAnimationBuilder";
 import type MoranaAppContextType from "@root/types/MoranaAppContextType";
 import { MoranaAppContext } from "@root/core/context";
-import { defaultSlideNavAnimationBuilder } from "@root/navAnimationBuilder/defaults";
+import {
+    defaultSlideNavAnimationBuilder,
+    defaultFadeNavAnimationBuilder,
+} from "@root/navAnimationBuilder/defaults";
 
 interface MoranaAppProps {
-    navAnimationBuilder?: NavAnimationBuilder;
+    navAnimationBuilder?: "slide" | "fade" | NavAnimationBuilder;
 }
 
 export default function MoranaApp({
     children,
-    navAnimationBuilder = defaultSlideNavAnimationBuilder,
+    navAnimationBuilder = "slide",
 }: PropsWithChildren<MoranaAppProps>) {
     const [isNavbarVisible, setIsNavbarVisible] = useState(true);
 
+    const animationBuilder = useMemo(() => {
+        if (navAnimationBuilder === "fade") {
+            return defaultFadeNavAnimationBuilder;
+        }
+
+        if (navAnimationBuilder === "slide") {
+            return defaultSlideNavAnimationBuilder;
+        }
+
+        return navAnimationBuilder;
+    }, [navAnimationBuilder]);
+
     const values: MoranaAppContextType = useMemo(() => {
-        return { navAnimationBuilder, isNavbarVisible, setIsNavbarVisible };
-    }, [isNavbarVisible, navAnimationBuilder]);
+        return {
+            navAnimationBuilder: animationBuilder,
+            isNavbarVisible,
+            setIsNavbarVisible,
+        };
+    }, [isNavbarVisible, animationBuilder]);
 
     return (
         <MoranaAppContext.Provider value={values}>
